65ad2a28a15848114dd5a830265d3f3f63ac4c9b
Jamik <dzamikhidirov8@gmail.com>
2026-09-24 01:31:07 +0300

    Обновлен градл и добавлены необходимые модули в проект

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,13 +26,47 @@ rootProject.name = "Sona"
 include(":app")
 include(
     ":core",
+    ":core:audio",
     ":core:cache",
     ":core:camera",
     ":core:common",
     ":core:data",
     ":core:network",
-    ":core:uikit"
+    ":core:uikit",
+    ":core:worker"
 )
+
+include(
+    ":core:data:apps",
+    ":core:data:battery",
+    ":core:data:bluetooth",
+    ":core:data:browser",
+    ":core:data:clipboard",
+    ":core:data:contacts",
+    ":core:data:device",
+    ":core:data:display",
+    ":core:data:documents",
+    ":core:data:downloads",
+    ":core:data:gallery",
+    ":core:data:location",
+    ":core:data:media",
+    ":core:data:network",
+    ":core:data:notifications",
+    ":core:data:performance",
+    ":core:data:permissions",
+    ":core:data:sensors",
+    ":core:data:sms",
+    ":core:data:storage",
+    ":core:data:usage",
+    ":core:data:wifi",
+    )
+
+include(
+    ":core:network:api",
+    ":core:network:firebase",
+    ":core:network:supabase",
+)
+
 include(
     ":feature",
     ":feature:comments",
